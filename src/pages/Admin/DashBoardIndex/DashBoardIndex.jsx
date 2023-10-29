34fd00261bdabea108b0c0d75eab4b45845fae09
fhoinaski <vendasfhok@gmail.com>
2023-10-29 18:55:05 -0300

    pagina de vendas e de historico de vendas

--- src/pages/Admin/DashBoardIndex/DashBoardIndex.jsx
+++ src/pages/Admin/DashBoardIndex/DashBoardIndex.jsx
@@ -1,11 +1,60 @@
 import styles from './DashBoardIndex.module.css'
+import React, { useState, useEffect } from "react";
+import useAuth from "../../../hooks/useAuth";
+import useApi from "../../../hooks/useApi";
 
-const DashBoardIndex = () => {   
-    return (
+const DashBoardIndex = () => {
+  const { token } = useAuth();
+  const [salesData, setSalesData] = useState([]);
+  const { getSalesDashboard } = useApi();
+
+  useEffect(() => {
+    const fetchSalesData = async () => {
+      const data = await getSalesDashboard(token);
+      setSalesData(data);
+    };
+    fetchSalesData();
+  }, [token]);
+
+  const getTotalSales = () => {
+    if (!Array.isArray(salesData)) {
+      return 0;
+    }
+  
+    let totalSales = 0;
+    salesData.forEach((sale) => {
+      totalSales += sale.total || 0;
+    });
+    return totalSales;
+  };
+  
+  const getTotalProductsSold = () => {
+    if (!Array.isArray(salesData)) {
+      return 0;
+    }
+  
+    let totalProductsSold = 0;
+    salesData.forEach((sale) => {
+      totalProductsSold += sale.products.reduce((acc, product) => acc + (product.quantity || 0), 0);
+    });
+    return totalProductsSold;
+  };
+
+  return (
+    <div className={styles.card}>
+      <h2 >Relatório de Vendas</h2>
+      <div className={styles.cardContainer}>
         <div>
-            <h1 className={styles.text}>Home</h1>
+          <h3>Total de Vendas  = {getTotalSales()}</h3>
+    
         </div>
-    )
-}
+        <div>
+          <h3>Produtos Vendidos  = {getTotalProductsSold()}</h3>
+        
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default DashBoardIndex
+export default DashBoardIndex;
--- src/pages/Admin/Sales/Sales.jsx
+++ src/pages/Admin/Sales/Sales.jsx
@@ -1,7 +1,38 @@
+import React, { useState, useEffect } from 'react' 
+import useAuth  from '../../../hooks/useAuth'
+import useApi from '../../../hooks/useApi'
+import styles from "./Sales.module.css"
+
 const Sales = () => {
+    const [sales, setSales] = useState([])
+    const { getAdminSales } = useApi()
+    const { token } = useAuth();
+
+    useEffect(() => {
+        const fetchSales = async () => {
+            const data = await getAdminSales(token)
+            setSales(data)
+        }
+        fetchSales()
+    }, [token])
+
     return (
         <div>
+            <div className={styles.titulo}>
             <h1>Vendas</h1>
+            </div>
+
+            <div className={styles.cardSales}>
+            {sales.map((sale) => (
+                <div className={styles.cardSalesBloco} key={sale.productId}>
+                    <h2>{sale.name}</h2>
+                    <img src={sale.imageLink} alt={sale.name} />
+                    <p>Quantidade de itens vendidos: {sale.amountBuy}</p>
+                    <p>Preço unitário: R${sale.unitPrice.toFixed(2)}</p>
+                    <p>Valor total das vendas: R${sale.total.toFixed(2)}</p>
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
