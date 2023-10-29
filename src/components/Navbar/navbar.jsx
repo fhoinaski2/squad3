191821a691e72d5dd2f8f0645b5b3376d8efbe91
fhoinaski <vendasfhok@gmail.com>
2023-10-29 17:46:53 -0300

    correção grafia navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
-import styles from "./navbar.module.css"
+import styles from "./Navbar.module.css"
 import useFetchProducts from '../../hooks/useFecthProducts';
 
 
