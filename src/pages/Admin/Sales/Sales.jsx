5b83c961b6cb18d7a64861380301e45bf8c43dca
fhoinaski <vendasfhok@gmail.com>
2023-10-29 18:59:37 -0300

    coment map de vendas

--- src/pages/Admin/Sales/Sales.jsx
+++ src/pages/Admin/Sales/Sales.jsx
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from 'react' 
-import useAuth  from '../../../hooks/useAuth'
 import useApi from '../../../hooks/useApi'
 import styles from "./Sales.module.css"
 
 const Sales = () => {
     const [sales, setSales] = useState([])
     const { getAdminSales } = useApi()
-    const { token } = useAuth();
+    
 
     useEffect(() => {
         const fetchSales = async () => {
-            const data = await getAdminSales(token)
+            const data = await getAdminSales()
             setSales(data)
         }
         fetchSales()
-    }, [token])
+    }, [])
 
     return (
         <div>
@@ -23,7 +22,7 @@ const Sales = () => {
             </div>
 
             <div className={styles.cardSales}>
-            {sales.map((sale) => (
+            {/* {sales.map((sale) => (
                 <div className={styles.cardSalesBloco} key={sale.productId}>
                     <h2>{sale.name}</h2>
                     <img src={sale.imageLink} alt={sale.name} />
@@ -31,7 +30,7 @@ const Sales = () => {
                     <p>Preço unitário: R${sale.unitPrice.toFixed(2)}</p>
                     <p>Valor total das vendas: R${sale.total.toFixed(2)}</p>
                 </div>
-            ))}
+            ))} */}
             </div>
         </div>
     )
